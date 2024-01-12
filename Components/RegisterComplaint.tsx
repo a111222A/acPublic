5a81a498cb3bea13764337e81b6cc827b55716f9
avik <mynameisavikghosh@gmail.com>
2024-01-12 16:11:30 +0530

    test by avik

--- Components/RegisterComplaint.tsx
+++ Components/RegisterComplaint.tsx
@@ -1,11 +1,39 @@
 import React from "react";
-import { Text, View } from 'react-native';
+import { Text, View,StyleSheet,TextInput,Button } from 'react-native';
+
 const RegisterComplaint = ()=>{
     return(
-        <View>
-            <Text>RegisterComplaint------------</Text>
+        <View style={styles.container}>
+            <TextInput
+        style={styles.input}
+        placeholder="Your Name"        
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Your Email"
+        keyboardType="email-address"     
+      />
+      <TextInput
+        style={[styles.input, { height: 100 }]}
+        placeholder="Enter your complaint here"  
+      />
+      {/*<Button title="Submit Complaint"  />*/}
+      
+            
         </View>
 
     )
 };
+const styles = StyleSheet.create({
+    container: {
+      padding: 20,
+    },
+    input: {
+      height: 40,
+      borderColor: '#000',
+      borderWidth: 1,
+      marginBottom: 10,
+      padding: 10,
+    },
+  });
 export default RegisterComplaint;
