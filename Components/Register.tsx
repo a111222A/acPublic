c4c332222e16e5e52ee6a9946de32a348817f3f6
biswajitdas4545 <biswajitdas4545@gmail.com>
2024-01-14 19:39:25 +0530

    registration page api implement

--- Components/Register.tsx
+++ Components/Register.tsx
@@ -1,17 +1,92 @@
 import React, { Component, useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Button, ScrollView, TouchableHighlight, } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, TouchableHighlight,Modal,Pressable } from 'react-native'
 import DropDownPicker from 'react-native-dropdown-picker';
 
 import DatePicker from 'react-native-date-picker'
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { BASEURL } from '../Components/Constant';
 
 const Register = ({ navigation }: { navigation: any }) => {
+    const [isModalVisible, setModalVisible] = useState(false);
+    
+    const [name,setName] = useState('');
+    const [mobileNumber,setMobileNumber] = useState('');
+    const [email,setEmail] = useState('');
+    const [pineCode,setPineCode] = useState('');
+    const [address,setAddress] = useState('');
+    const [password,setPassword] = useState('');
 
+    const [nameError,setNameError] = useState(false);
+    const [mobileNumberError,setMobileNumberError] = useState(false);
+    const [emailError,setEmailError] = useState(false);
+    const [pineCodeError,setPineCodeError] = useState(false);
+    const [addressError,setAddressError] = useState(false);
+    const [passwordError,setPasswordError] = useState(false);
 
+    const Register =async()=>{
+        
+        if(!name){
+            setNameError(true);
+        }else{
+            setNameError(false);
+        }
+        
+        if(!mobileNumber){
+            setMobileNumberError(true);
+        }else{
+            setMobileNumberError(false);
+        }
+        if(!email){
+            setEmailError(true);
+        }else{
+            setEmailError(false);
+        }
+        if(!pineCode){
+            setPineCodeError(true);
+        }else{
+            setPineCodeError(false);
+        }
+        if(!address){
+            setAddressError(true);
+        }else{
+            setAddressError(false);
+        }
+        if(!password){
+            setPasswordError(true);
+        }else{
+            setPasswordError(false);
+        } 
 
-    const Register = () => {
-        console.warn("registerSucess  method call");
-        navigation.navigate('Login');
+        if(!name || !mobileNumber || !email || !pineCode || !address || !password){
+            return false;
+        } 
+         try {
+            const registrationData = {
+                name: name,
+                mobileNumber:mobileNumber,
+                email:email,
+                address:address,
+                pine_code:pineCode,
+                password:password
+              };
+              //console.warn(registrationData);
+            const response = await fetch(BASEURL + '/technicianRegister',{
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(registrationData),
+            });
+            const result = await response.json();
+            if (result.success) {
+                setModalVisible(true);
+                //setData(result.data);
+              } else {
+                console.error('API request failed:', result.message);
+              }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        } 
     }
     {/** **/ }
     const [open, setOpen] = useState(false);
@@ -21,82 +96,59 @@ const Register = ({ navigation }: { navigation: any }) => {
         { label: 'MIS.', value: 'MIS.' },
         { label: 'MS.', value: 'MS.' },
     ]);
-    
+    const closeModal = () => {
+        setModalVisible(false);
+        navigation.navigate('Login');
+      };
 
 
     return (
         <View style={{ flex: 1, }}>
+            <View style={style.container}>
+            <Modal
+        animationType="slide"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={closeModal}
+        
+      >
+        <View style={style.modalContainer}>
+          <View style={style.modalContent}>
+            <Text>Registration Successful!</Text>
+            <Button title="Close" onPress={closeModal} style={style.modalButton} />
+          </View>
+        </View>
+      </Modal>
+            </View>
             <ScrollView>
-            <Text style={{ fontSize: 20, color: 'black' }}>  Title:</Text>
-                <View
-                    style={{
-                        flex: 1,
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        paddingHorizontal: 15,
-                        margin: 10
-                    }}>
-                    <DropDownPicker
-                        open={open}
-                        value={value}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setValue}
-                        setItems={setItems}
-                        placeholder={'Choose '}
-                        style={{ borderWidth: 2, borderColor: '#000', }}
-                    />
-                </View>
+                <Text style={{ fontSize: 20, color: 'black' }}>  Name :</Text>
+                <TextInput placeholder=' Enter Your First Name' value={name} onChangeText={(text)=>setName(text)} style={style.InputBox} />
+                {nameError?<Text style={style.errorMessage}> Please Enter Your First Name </Text>:''}  
 
-
-                <Text style={{ fontSize: 20, color: 'black' }}>  First name :</Text>
-                <TextInput placeholder=' Enter Your Nane' style={style.InputBox} />
-
-                <Text style={{ fontSize: 20, color: 'black' }}>  Last name :</Text>
-                <TextInput placeholder=' Enter Your Nane' style={style.InputBox} />
-
+                
                 <Text style={{ fontSize: 20, color: 'black' }}>  Mobile Number :</Text>
-                <TextInput placeholder=' Enter Your Mobile No' style={style.InputBox} />
+                <TextInput placeholder=' Enter Your Mobile No' value={mobileNumber} onChangeText={(text)=>setMobileNumber(text)} style={style.InputBox} />
+                {mobileNumberError?<Text style={style.errorMessage}> Please Enter Mobile Number </Text>:''}
 
                 <Text style={{ fontSize: 20, color: 'black' }}>  Email ID :</Text>
-                <TextInput placeholder=' Enter Your Email ID' style={style.InputBox} />
+                <TextInput placeholder=' Enter Your Email ID' value={email} onChangeText={(text)=>setEmail(text)} style={style.InputBox} />
+                {emailError?<Text style={style.errorMessage}> Please Enter Email ID </Text>:''}
 
-                <Text style={{ fontSize: 20, color: 'black' }}>  Gender :</Text>
-                <View style={{flex:1,flexDirection:'row'}}>
-                        <TouchableOpacity>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <View style={style.radio}>
-                                    <View style={style.fil}></View>
-                                </View>
-                                <Text style={style.text}>
-                                    Male
-                                </Text>
-                            </View>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <View style={style.radio}>
-                                    <View style={style.fil}></View>
-                                </View>
-                                <Text style={style.text}>
-                                    Female
-                                </Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
 
-                <Text style={{ fontSize: 20, color: 'black' }}>  Address1 :</Text>
-                <TextInput placeholder=' Enter Your Address1' style={style.InputBox} />
+                <Text style={{ fontSize: 20, color: 'black' }}>  Pine code :</Text>
+                <TextInput placeholder=' Enter Pine code' value={pineCode} onChangeText={(text)=>setPineCode(text)} style={style.InputBox} />
+                {pineCodeError?<Text style={style.errorMessage}> Please Enter Pine code </Text>:''}
 
-                <Text style={{ fontSize: 20, color: 'black' }}>  Address2 :</Text>
-                <TextInput placeholder=' Enter Your Address2' style={style.InputBox} />
+               
+
+                <Text style={{ fontSize: 20, color: 'black' }}>  Address :</Text>
+                <TextInput placeholder=' Enter Your Address' value={address} onChangeText={(text)=>setAddress(text)} style={style.InputBox} />
+                {addressError?<Text style={style.errorMessage}> Please Enter Address </Text>:''}
 
 
                 <Text style={{ fontSize: 20, color: 'black' }}>  Password :</Text>
-                <TextInput placeholder='Enter Password ' secureTextEntry={true} style={style.InputBox} />
-
-                <Text style={{ fontSize: 20, color: 'black' }}>  Confrim Password :</Text>
-                <TextInput placeholder='Enter confirm Password ' secureTextEntry={true} style={style.InputBox} />
+                <TextInput placeholder='Enter Password ' value={password} onChangeText={(text)=>setPassword(text)} secureTextEntry={true} style={style.InputBox} />
+                {passwordError?<Text style={style.errorMessage}> Please Enter Password </Text>:''}
 
                 <View style={{ margin: 6 }}>
                     <TouchableHighlight onPress={() => Register()}>
@@ -119,6 +171,7 @@ const style = StyleSheet.create({
         borderWidth: 2,
         fontSize: 20,
         margin: 10,
+        marginBottom:5
     },
     Button: {
         backgroundColor: "#008080",
@@ -152,9 +205,37 @@ const style = StyleSheet.create({
         backgroundColor: '#000',
         borderRadius: 20,
         margin: 2
-
-
-    }
+    },
+    errorMessage: {
+        fontSize: 20, 
+        color: 'red'
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      modalContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // semi-transparent background
+      },
+      modalContent: {
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+      },
+      modalButton: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: 'blue',
+        borderRadius: 5,
+      },
+      modalButtonText: {
+        color: 'white',
+      },
 })
 
 export default Register;
