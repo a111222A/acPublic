bd196a73060947f135d756784c5df75d9bbf21af
avik <mynameisavikghosh@gmail.com>
2024-01-20 20:38:26 +0530

    bj

--- Components/RegisterComplaint.tsx
+++ Components/RegisterComplaint.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useRef } from 'react';
 import { Text, View,StyleSheet,TextInput,Button } from 'react-native';
+import DocumentPicker from "react-native-document-picker";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Entypo from 'react-native-vector-icons/Entypo';
+import { RNCamera } from 'react-native-camera';
+
 
 const RegisterComplaint = ()=>{
+  const SelectDocumernt = async () => {
+    try {
+      const doc = await DocumentPicker.pick({
+        type: [DocumentPicker.types.images]
+      });
+
+    } catch (err) {
+      if (DocumentPicker.isCancel()) {
+        console.log("User cancelled the upload");
+      }
+      else {
+        console.log(err)
+      }
+
+    }
+  }
+  const cameraRef = useRef(null);
+
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      console.log(data.uri);
+    }
+  };
+
     return(
         <View style={styles.container}>
             <TextInput
@@ -17,8 +48,34 @@ const RegisterComplaint = ()=>{
         style={[styles.input, { height: 100 }]}
         placeholder="Enter your complaint here"  
       />
-      {/*<Button title="Submit Complaint"  />*/}
-      
+      {/*<Button title="Submit Complaint"  />
+      <Button title="updete" onPress={SelectDocumernt} />*/}
+      <Text >
+        
+      </Text>
+      <View style={{justifyContent:'center',alignItems:'center'}}>
+      <Entypo name='upload' color={'#1b868e'} size={30} />
+      <Text style={{color:'black'}}>Drop your file here </Text>
+        
+    
+      <TouchableOpacity onPress={SelectDocumernt} style={styles.bnt}>
+        <Text style={{color:'#ffffff'}}>Updete</Text>
+      </TouchableOpacity>
+      </View>
+
+      <View style={{ flex: 1 }}>
+      <RNCamera
+        ref={cameraRef}
+        style={{ flex: 1 }}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
+      />
+      <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={takePicture}>
+          <Text style={{ fontSize: 14 }}> SNAP </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
             
         </View>
 
@@ -34,6 +91,14 @@ const styles = StyleSheet.create({
       borderWidth: 1,
       marginBottom: 10,
       padding: 10,
+    },
+    bnt:{
+      alignItems:'center',
+      justifyContent:'center',
+      backgroundColor:'#008080',
+      height:40,
+      width:130,
+
     },
   });
 export default RegisterComplaint;
